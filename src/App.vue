0febd8034cd9275db9c9b673052b4c3df253a118
Tyler Carver <tbcarver2@gmail.com>
2021-10-12 09:38:24 -0600

    Minor clean up.

--- src/App.vue
+++ src/App.vue
@@ -19,10 +19,10 @@
 </template>
 
 <script>
-import Form from './components/Form.vue'
+import Form from "./components/Form.vue"
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Form
   }
